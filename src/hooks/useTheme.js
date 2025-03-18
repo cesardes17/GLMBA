43c4799302298d20935eee6bbb400bd49eede13d
Cesar Delgado <cjdsgithub@gmail.com>
2025-03-18 13:18:30 +0000

    actualizado error en useEffect, usando useMemo

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useMemo,
+} from "react";
 import { useColorScheme } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { themes } from "../theme/themes";
@@ -8,22 +14,21 @@ const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const systemColorScheme = useColorScheme();
-  const [userPreference, setUserPreference] = useState("system"); // Por defecto "system"
-  const [statusBarStyle, setStatusBarStyle] = useState("dark"); // Estado para actualizar la StatusBar
+  const [userPreference, setUserPreference] = useState("system");
+  const [statusBarStyle, setStatusBarStyle] = useState("dark");
 
-  // Obtener el tema correcto según la preferencia del usuario
-  const getTheme = () => {
+  // Memoizar getTheme para evitar recreaciones innecesarias
+  const theme = useMemo(() => {
     if (userPreference === "system") {
       return systemColorScheme === "dark" ? themes.dark : themes.light;
     }
     return userPreference === "dark" ? themes.dark : themes.light;
-  };
-
-  // Actualizar `StatusBar` cuando cambie el esquema de colores
-  useEffect(() => {
-    const newStatusBarStyle = getTheme() === themes.dark ? "light" : "dark";
-    setStatusBarStyle(newStatusBarStyle);
   }, [userPreference, systemColorScheme]);
+
+  // Actualizar `StatusBar` cuando cambia el tema
+  useEffect(() => {
+    setStatusBarStyle(theme === themes.dark ? "light" : "dark");
+  }, [theme]);
 
   // Cargar la preferencia del usuario desde AsyncStorage al iniciar la app
   useEffect(() => {
@@ -31,7 +36,7 @@ export const ThemeProvider = ({ children }) => {
       try {
         const storedPreference = await AsyncStorage.getItem("themePreference");
         if (storedPreference) {
-          setUserPreference(storedPreference); // Aplica la preferencia guardada
+          setUserPreference(storedPreference);
         }
       } catch (error) {
         console.error("Error al cargar la preferencia del tema:", error);
@@ -53,9 +58,7 @@ export const ThemeProvider = ({ children }) => {
   }, [userPreference]);
 
   return (
-    <ThemeContext.Provider
-      value={{ theme: getTheme(), userPreference, setUserPreference }}
-    >
+    <ThemeContext.Provider value={{ theme, userPreference, setUserPreference }}>
       <StatusBar style={statusBarStyle} />
       {children}
     </ThemeContext.Provider>
